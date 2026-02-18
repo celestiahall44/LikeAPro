f6674d5a7f943f21d097b232bf49e29e6a9a772c
celestiahall44 <celestia.hall44@gmail.com>
2026-02-18 07:53:32 -0700

    js

--- index.js
+++ index.js
@@ -4,25 +4,23 @@
 
 function contact(event) {
     event.preventDefault();
+    const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success"); 
+    loading.classList += " modal__overlay--visible";
     emailjs
         .sendForm(
            'service_4cp26t8',
            'template_wtxo4hk',
            event.target,
            'PunT1yUjK2-xsBH1z'
-        )
-        .then(() => {
-            loading.classList.remove("modal__overlay--visible");
-            success.classList +=" modal__overlay--visible";
-        })
-        .catch(() )
-    
-const loading = document.querySelector(".modal__overlay--loading");
-const success = document.querySelector(".modal__overlay--success");
-loading.classList =+ " modal__overlay--visible"
-setTimeout(() => {
-    loading.classList.remove("modal__overlay--visible");
-    success.classList =+ " modal__overlay--visible";
-    console.log('it worked 1')
-}, 1000);
+    ).then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly at celestia.hall44@gmail.com"
+         );
+    })
 }
+    
